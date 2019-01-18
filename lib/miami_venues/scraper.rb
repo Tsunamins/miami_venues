937f63e7f0ccdd68b0a901bde904976b551ad72c
Alison <reillyamr@gmail.com>
2019-01-18 17:46:16 -0500

    started arsht center scraper

--- lib/miami_venues/scraper.rb
+++ lib/miami_venues/scraper.rb
@@ -1,3 +1,18 @@
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
+
+class MiamiVenues::Scraper
+  attr_accessor :@events
+
+  @events = []
+
+  def arsht_events
+    events = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    puts events.css("ul#eventlist")
+  end
+
+
+
+end
