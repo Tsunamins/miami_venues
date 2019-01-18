673b6cd0bb42bb983134dbe4818d39f62da94643
Alison Reilly <reillyamr@gmail.com>
2019-01-18 23:20:28 +0000

    worked on arsht events method

--- lib/miami_venues/scraper.rb
+++ lib/miami_venues/scraper.rb
@@ -2,15 +2,16 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-class MiamiVenues::Scraper
-  attr_accessor :@events
-
+class Scraper
+  attr_accessor :events
   @events = []
 
   def arsht_events
     events = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-    puts events.css("ul#eventlist")
+    
+    
+    events.css("ul#eventlist")
+    
   end
 
 
