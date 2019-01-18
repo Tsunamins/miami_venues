defd20f8e2a20f170711086eecda2b5f1c6eb77a
Alison Reilly <reillyamr@gmail.com>
2019-01-18 19:10:10 +0000

    first commit

---
+++ lib/miami_venues.rb
@@ -0,0 +1,6 @@
+require "miami_venues/version"
+
+module MiamiVenues
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/miami_venues/version.rb
@@ -0,0 +1,3 @@
+module MiamiVenues
+  VERSION = "0.1.0"
+end
