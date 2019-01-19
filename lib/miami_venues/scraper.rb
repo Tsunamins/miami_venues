7a7cd1c36df7bf8d1b0a401c64f8bba9caa5b577
Alison Reilly <reillyamr@gmail.com>
2019-01-19 22:26:35 +0000

    enough changes to connect with arsht site

--- lib/miami_venues/scraper.rb
+++ lib/miami_venues/scraper.rb
@@ -3,15 +3,15 @@ require 'pry'
 require 'nokogiri'
 
 class MiamiVenues::Scraper
-  attr_accessor :events
-  @events = []
+  # attr_accessor :events, :arsht_events
+  # @events = []
 
   def arsht_events
    
-    events = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    events = Nokogiri::HTML(open("http://www.arshtcenter.org/Tickets/Buy-Tickets-Now/"))
 
     
-    puts events.css("ul#eventlist")
+    puts events.css("div.performanceList")
     
 
   end
