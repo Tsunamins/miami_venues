4b3c97573f420c76ad13b1e2621a2dc1b6206e23
Alison <reillyamr@gmail.com>
2019-01-23 21:04:27 -0500

    added perez main page scraper and method to format dates and create range of dates

--- lib/miami_venues/scraper.rb
+++ lib/miami_venues/scraper.rb
@@ -3,26 +3,48 @@ require 'pry'
 require 'nokogiri'
 
 class MiamiVenues::Scraper
-  # attr_accessor :events, :arsht_events
-  # @events = []
-
-  def arsht_events
-<<<<<<< HEAD
-    #TODO change website here:
-    events = Nokogiri::HTML(open("http://www.arshtcenter.org/Tickets/Buy-Tickets-Now/"))
+  # attr_accessor :events
+  events = []
 
 
-    events.css("ul#eventList")
-=======
-   
-    events = Nokogiri::HTML(open("http://www.arshtcenter.org/Tickets/Buy-Tickets-Now/"))
+def perez_art_list
+  perez_a = Nokogiri::HTML(open("https://www.pamm.org/calendar"))
 
-    
-    puts events.css("div.performanceList")
-    
->>>>>>> 7a7cd1c36df7bf8d1b0a401c64f8bba9caa5b577
+  #find title of event, dates and url
 
+  perez_a.css("li div.inner").each do |find_detail|
+    events = {:event_name => find_detail.css("h4").text,
+      :date => [find_detail.css("span.meta").text],
+      :url => find_detail.css("a").attribute("href").value}
   end
+end
+
+puts events
+
+def standard_date
+  find_date = events[:date]
+  find_date.each do |date|
+    if date.include?("-")
+      date.split("-")
+      first_day = date[0]
+      last_day = date[1]
+      date_range = first_day..last_day
+      date_range.to_a
+      date_range.each do |change_format|
+        change_format.strftime('%a %d %b %Y')
+      end
+    elsif date.include?("-") == false
+      date.strftime('%a %d %b %Y')
+    end
+    end
+  end
+
+
+
+
+
+
+
 
 
 
