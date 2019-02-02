78589f6077a7321466777c9a77a1f85b40edda9b
Alison <reillyamr@gmail.com>
2019-02-02 12:41:34 -0500

    added more user interface to cli

--- lib/miami_venues/cli.rb
+++ lib/miami_venues/cli.rb
@@ -4,13 +4,29 @@
 
 
 class MiamiVenues::CLI
-  attr_accessor :arsht_events
+
   #code to see if all is setup correctly
   #called in bin findjobs to initially check
   def start
     puts "Hello"
-    
-    
+    puts "Do you want to find something to do in Miami today or another day?"
+    puts "Enter 1 for today, enter 2 for another day: "
+    user_input = gets.strip
+    user_choice = format_user_input(user_input)
+    if user_choice == "1"
+      puts "Here are today's events: "
+      #some method from eventmatch.rb to match
+    elsif user_choice == "2"
+      puts "Here are events of that day: "
+    else
+      puts "That is not an option, try again."
+    end
+
+
+
+
+
+
     #example of calling scraper method within here:
     #MiamiVenues::Scraper.scrape
   end
