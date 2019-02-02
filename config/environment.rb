c3448fa54bbe7435d6e08bdd1b464c368534cc0e
Alison <reillyamr@gmail.com>
2019-02-02 14:56:30 -0500

    added file requirement eventmatch

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,4 @@ require_relative '../lib/miami_venues/scraper'
 
 require_relative '../lib/miami_venues/cli'
 require_relative '../lib/miami_venues/version'
+require_relative '../lib/miami_venues/eventmatch'
