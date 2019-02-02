c84aa81c0ffa0a38ce1fbd0c87b42563dbbf582f
Alison <reillyamr@gmail.com>
2019-02-02 14:57:09 -0500

    started creating more specific cli

--- lib/miami_venues/cli.rb
+++ lib/miami_venues/cli.rb
@@ -2,6 +2,10 @@
 #is a means of namespacing so cli under any other user is specific
 #to this gem and not some other user's gem and environment setup
 
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+require 'date'
 
 class MiamiVenues::CLI
 
@@ -13,13 +17,20 @@ class MiamiVenues::CLI
     puts "Enter 1 for today, enter 2 for another day: "
     user_input = gets.strip
     user_choice = format_user_input(user_input)
+
     if user_choice == "1"
+      today = Time.new
+      format_today = today.strftime('%a %d %b %Y')
+      
+      puts format_today
+
       puts "Here are today's events: "
       #some method from eventmatch.rb to match
     elsif user_choice == "2"
       puts "Here are events of that day: "
     else
       puts "That is not an option, try again."
+      start
     end
 
 
@@ -29,5 +40,13 @@ class MiamiVenues::CLI
 
     #example of calling scraper method within here:
     #MiamiVenues::Scraper.scrape
+  end
+
+
+
+
+  def format_user_input(user_input)
+    user_string = user_input.to_s
+    return user_string
   end
 end
