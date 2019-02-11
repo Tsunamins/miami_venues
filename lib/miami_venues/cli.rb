bd2718ec024e2a52e00440484e4b1b4d1dbb3051
Alison <reillyamr@gmail.com>
2019-02-11 11:45:47 -0500

    added ability to find and create all events and start of display

--- lib/miami_venues/cli.rb
+++ lib/miami_venues/cli.rb
@@ -25,7 +25,7 @@ class MiamiVenues::CLI
       puts format_today
 
       puts "Here are today's events: "
-  
+
     elsif user_choice == "2"
       puts "Enter your date"
       puts "use a format such as: 2/15/2019, feb 15 2019, february 15, 2019"
@@ -36,23 +36,46 @@ class MiamiVenues::CLI
       puts "That is not an option, try again."
       start
     end
-
-
-
-
-
-
-    #example of calling scraper method within here:
-    #MiamiVenues::Scraper.scrape
   end
-  #helper method to change date each time
+
+
+
+
+
+  #find and create events based on scraper and event classes
+
+  def find_current_events
+    current_laser = MiamiVenues::Scraper.sci_museum_laser_fridays
+    MiamiVenues::Events.from_scraped_page(current_laser)
+    current_art = MiamiVenues::Scraper.perez_art_list
+    MiamiVenues::Events.from_scraped_page(current_art)
+  end
+
+  # def find_current_laser
+  #   current_laser = MiamiVenues::Scraper.sci_museum_laser_fridays
+  #   MiamiVenues::Events.from_scraped_page(current_laser)
+  #
+  # end
+  #
+  # def find_current_art
+  #   current_art = MiamiVenues::Scraper.perez_art_list
+  #   MiamiVenues::Events.from_scraped_page(current_art)
+  # end
+
+  def display_all
+    # find_current_art
+    # find_current_laser
+    find_current_events
+    all_events = MiamiVenues::Events.all
+    puts all_events
+  end
+
+  #helper method to change date
   def change_user_date(input_date)
     parsed_date = DateTime.parse("#{input_date}")
     use_date = parsed_date.strftime('%a %d %b %Y')
     puts use_date
   end
-
-
 
   #helper method to change user input as needed
   def format_user_input(user_input)
